34285fcee2f2e7dbeef427183ef2df1d3f4ffc8e
Alex Coventry <coventry@gmail.com>
2014-05-10 14:11:01 -0400

    Generator for random structures with extensive sharing

---
+++ test/src/test/graft/generators.cljs
@@ -0,0 +1,23 @@
+(ns test.generators
+  (:require [graft :as g]
+            [clojure.browser.repl]
+            [cemerick.cljs.test :as t]
+            [cemerick.double-check.generators :as gen]
+            [cemerick.double-check.properties :as prop :include-macros true])
+  (:use-macros [cemerick.cljs.test :only (is deftest testing)]))
+
+(defn structured* [size]
+  (if (not (pos? size))
+    gen/any
+    (let [new-size (* (quot size 4) 3)
+          smaller-structure (gen/resize new-size (gen/sized structured))]
+      (gen/bind (gen/frequency
+                 [[1 gen/simple-type]
+                  [10 (gen/vector smaller-structure)]])
+                (fn [%]
+                  (if (and (coll? %) (> (count %) 0))
+                    (gen/sized (gen/sized-container (gen/elements %)))
+                    (gen/return %)))))))
+
+(defn structured []
+  (gen/sized structured*))
