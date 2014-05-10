8fa8881939b63b6618dd5273c227c14ec61859fc
Alex Coventry <coventry@gmail.com>
2014-05-10 19:11:32 -0400

    Do topsort on first pass, during compression phase

--- src/graft/graft.cljs
+++ src/graft/graft.cljs
@@ -4,30 +4,77 @@
 (def id-prop "__cljs_graft_id__")
 
 (defn get-id! [obj]
+  "Assign an ID to OBJ as a javascript property if it does not already
+  have one.  Used to track identity of substructures during
+  compression."
   (if-let [id (aget obj id-prop)]
     id
     (aset obj id-prop (gensym id-prop))))
 
-(defn add-ref! [duplicates seen ref]
+;; XXX Since I'm using an atom anyway, probably better to make this a
+;; set of JS objects which I bash in-place.
+(def duplicate-start
+  {;; Used to keep track of when we come across something we've seen
+   ;; before, so that we can identify duplicates, and TOPSORT the
+   ;; dependencies between them.
+   :seen {}
+   :seen-count 0
+   ;; Used to track the actual duplicates
+   :duplicates {}})
+
+(defn add-unseen [duplicates id]
+  (-> duplicates
+      (update-in [:seen] assoc id (:seen-count duplicates))
+      (update-in [:seen-count] inc)))
+
+(defn add-ref!* [duplicates ref]
   (let [id (get-id! ref)]
-    (if (not (@seen id))
-      (do (swap! seen conj id)
-          ;; Continue the traversal
-          ref)
-      (do (swap! duplicates assoc id ref)
-          ;; Don't follow this node further
-          nil))))
+    (if (not (contains? (:seen duplicates) id))
+      (add-unseen duplicates id)
+      (update-in duplicates [:duplicates] assoc id ref))))
+
+(defn add-ref! [duplicates ref]
+  (let [current-count (:seen-count duplicates)
+        new-duplicates (swap! duplicates add-ref!* ref)]
+    (if (< current-count (:seen-count new-duplicates))
+      ;; Was a previously seen object, keep descending tree
+      ref)))
 
 (defn get-duplicates [obj]
-  (let [duplicates (atom nil)
-        seen (atom #{})
-        add-ref! (partial add-ref! duplicates seen)]
+  (let [duplicates (atom duplicate-start)
+        add-ref! (partial add-ref! duplicates)]
     (w/prewalk #(when (coll? %) (add-ref! %)) obj)
     @duplicates))
 
+;; apply to the (key/val swapped) duplicates map itself to deflate it?
 (defn prune-duplicates [obj duplicates]
+  "Returns OBJ with all instances of substructures whose ids (via
+  GET-ID!) are keys in DUPLICATES replaced by corresponding values."
   (let [prune #(let [id (get-id! %)] (if (duplicates id) id %))]
     (w/prewalk prune obj)))
+
+(defn find-grafts [obj duplicates]
+  "Walk OBJ, looking for values which are keys in DUPLICATES, and replacing them with ")
+
+(defn reflate* [duplicates reflated id obj seen]
+  )
+
+(defn reflate [duplicates reflated id obj]
+  (if (contains? reflated id)
+    reflated
+    (trampoline reflate* duplicates reflated id obj #{})))
+
+(defn reflate-duplicates [duplicates]
+  "Recursively replace all instances of ids referred to by vals of
+  DUPLICATES with their given values.  SEEN-IDS is used to keep track
+  of cyclic dependencies between DUPLICATES... XXX"
+  (reduce-kv (partial reflate duplicates) {} duplicates)
+  ;; Imperative version, gets me nothing.
+  #_(let [reflated (atom nil)]
+    (doseq [[id obj] duplicates
+            :when (not (contains? @reflated id))
+            :let [reflated-obj (reflate duplicates reflated obj)]]
+      (swap! reflated assoc id reflated-obj))))
 
 (defn graft-duplicates [obj duplicates]
   (w/prewalk-replace duplicates obj))
--- test/src/test/graft.cljs
+++ test/src/test/graft.cljs
@@ -1,6 +1,5 @@
 (ns test.graft
   (:require [graft :as g]
-            
             [test.generators :as t-gen]
             [clojure.browser.repl]
             [cemerick.cljs.test :as t]
@@ -8,17 +7,26 @@
             [cemerick.double-check.properties :as prop :include-macros true])
   (:use-macros [cemerick.cljs.test :only (is deftest testing)]))
 
+(deftest assoc-smart-about-identity
+  (let [m {} n {m m} nn (assoc n m m)]
+    (is (identical? n nn)
+        "assoc does nothing when requested key/val are (identical?)")))
+
 (deftest get-duplicates 
   (let [foo [1 2]
         bar [1 2]
         composite [foo bar foo]
-        duplicates (g/get-duplicates composite)
-        seen? #(seq (filter (partial identical? %1) (vals duplicates)))
+        duplicates-result (g/get-duplicates composite)
+        duplicates (:duplicates duplicates-result)
+        dup? #(seq (filter (partial identical? %1) (vals duplicates)))
         pruned (g/prune-duplicates composite duplicates)]
-    (is (seen? foo)
+    (is (dup? foo)
         "get-duplicates should see substructure with multiple references")
-    (is (not (seen? bar))
+    (is (not (dup? bar))
         "get-duplicates should ignore unique substructure")
+    (is (< (get-in duplicates-result [:seen (g/get-id! foo)])
+           (get-in duplicates-result [:seen (g/get-id! bar)]))
+        "foo should be seen before bar")
     (is (some (partial = (g/get-id! foo)) (flatten pruned))
         (str "the reference to the duplicate structure should appear in "
              "the pruned version"))
@@ -26,3 +34,10 @@
         "there should be no reference to the unique structure")
     (is (= composite (g/graft-duplicates pruned duplicates))
         "reconstruction should result in equivalent structure")))
+
+(get-duplicates)
+
+(defn round-trip [s]
+  )
+
+(assoc-smart-about-identity)
